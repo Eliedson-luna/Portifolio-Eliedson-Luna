9ac589f59fe0e4579decbe05926de4ba0e2c1e38
Eliedson <goncalveseliedson@gmail.com>
2025-04-15 07:12:18 -0300

    estilizaçao da pagina de projetos

--- src/components/pages/projetos/projectList/components.tsx
+++ src/components/pages/projetos/projectList/components.tsx
@@ -31,7 +31,7 @@ export default function ProjectList() {
         <div id="project-box" className="p-[1%]">
             {
                 projects.map((proj: any, index: number) => (
-                    <article className="project flex flex-row h-full w-[100%]" id={`Project-${index + 1}`} key={index}>
+                    <article className="project flex flex-row h-full w-[100%] border-black border-b-2 p-3 hover:cursor-pointer hover:border-gray-700" id={`Project-${index + 1}`} key={index}>
                         <div className="w-[70%]">
                             <h3 className="mb-3">{proj.title}</h3>
                             <div id="text">
@@ -40,21 +40,23 @@ export default function ProjectList() {
                                 ))}
                             </div>
                         </div>
-                        <div className="w-[30%] ml-1 bg-amber-100">
-                            <Carrousel>
-                                {
-                                    proj.img.map((link: string, n: number) => (
+                        <div className="w-[30%] p-1 bg-amber-100">
+                            <div className="w-[100%] flex justify-center align-middle">
+                                <Carrousel>
+                                    {
+                                        proj.img.map((link: string, n: number) => (
 
-                                        <Image
-                                            key={n}
-                                            src={link}
-                                            alt="Project Image"
-                                            layout="fill"
-                                            className="object-center"
-                                        />
-                                    ))
-                                }
-                            </Carrousel>
+                                            <Image
+                                                key={n}
+                                                src={link}
+                                                alt="Project Image"
+                                                height={100}
+                                                width={100}
+                                            />
+                                        ))
+                                    }
+                                </Carrousel>
+                            </div>
                         </div>
                     </article>
                 )
