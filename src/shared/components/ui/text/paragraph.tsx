f3c5364e1029359d151a0abe455338ed7f5ea8e0
Eliedson <goncalveseliedson@gmail.com>
2025-11-07 09:27:48 -0300

    refactor: corrigido o prop de alinhamento de texto

--- src/shared/components/ui/text/paragraph.tsx
+++ src/shared/components/ui/text/paragraph.tsx
@@ -2,10 +2,17 @@ type ParagraphProps = React.HTMLAttributes<HTMLParagraphElement> & {
     align?: 'left' | 'center' | 'right' | 'justify';
 }
 
-export function Paragraph({ children, className, align }: ParagraphProps) {
+export function Paragraph({ children, className, align = "left" }: ParagraphProps) {
+    const alignTypes: Record<'left' | 'center' | 'right' | 'justify', string> = {
+        justify: 'text-justify',
+        left: 'text-left',
+        right: 'text-right',
+        center: 'text-center'
+    }
+
     const classProps = `
             ${className} 
-            text-${align} 
+            ${alignTypes[align] ?? ''} 
             m-2
             text-text
             text-size-paragraph
