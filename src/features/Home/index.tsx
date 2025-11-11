4d3886c473cf3ec8014122472a9ac32b646ad0b3
Eliedson <goncalveseliedson@gmail.com>
2025-11-11 07:37:46 -0300

    remoção de tags desnecessarias

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -3,12 +3,10 @@ import TopProjects from "./components/topProjects";
 
 export default function HomepageBody() {
     return (
-        <>
         <div className="h-[90vh] flex flex-col justify-around">
                 <HomePageMenu />
                 <TopProjects />
         </div>
-        </>
     )
 }
 
