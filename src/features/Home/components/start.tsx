877655389f169d2957db44224c68456f038546f5
Eliedson <goncalveseliedson@gmail.com>
2026-08-03 17:08:21 -0300

    feat: new header and start page styles

--- src/features/Home/components/start.tsx
+++ src/features/Home/components/start.tsx
@@ -1,5 +1,13 @@
-import { Paragraph } from "@/shared/components/ui/text/paragraph";
-import { Title } from "@/shared/components/ui/text/title";
+'use client'
+import GenericButton from "@/shared/components/ui/buttons/genericButton";
+import { SubTitle, Title } from "@/shared/components/ui/text/title";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 
 export default function HomePageStart() {
     return (
@@ -13,34 +21,73 @@ export default function HomePageStart() {
                     w-full h-full 
                     ">
             <div className="flex w-full sm:w-[60%] sm:h-full">
-                <div className="flex flex-col justify-center place-content-start">
-                    <Title className="mb-10">
-                        ELIEDSON LUNA,
-                    </Title>
-                    <Title>
-                        DESENVOLVIMENTO DE SOFTWARE.
-                    </Title>
-                </div>
+                <RightContainer />
             </div>
-            <div className="flex flex-row sm:flex-col justify-center w-[85vw] sm:w-[25%] ">
-                <div className="
-                    flex flex-col sm:flex-col 
-                    sm:justify-center 
-                    gap-5 
-                    sm:border-l-border-subcont sm:border-l  border-t-border-subcont not-sm:border-t
-                    sm:h-[70%] 
-                    pt-10 sm:pl-5">
-                    <Paragraph align="justify">
-                        Opa tudo bom? Seja bem vindo ao DEV LUNA, meu portifólio profissional!
-                    </Paragraph>
-                    <Paragraph align="justify">
-                        Me chamo Eliedson e te convido à conhecer um pouco do meu trabalho.
-                    </Paragraph>
-                    <Paragraph align="justify">
-                        Sinta-se à vontade para navegar pelo meu site!
-                    </Paragraph>
-                </div>
+            {/* <div className="flex flex-row sm:flex-col justify-center w-[85vw] sm:w-[25%] ">
+                <LeftContainer />
+            </div> */}
+        </div>
+    )
+}
+
+const RightContainer = () => {
+    const router = useRouter();
+
+    const goTo = (location: string): void => {
+        router.push(location);
+
+    }
+
+    return (
+        <div className="flex flex-col justify-center place-content-start gap-15">
+            <div>
+                <Title className="mb-10">
+                    Eliedson Luna,
+                </Title>
+                <Title>
+                    Desenvolvedor <span className="text-gradient font-extrabold">FRONTEND</span>
+                </Title>
+            </div>
+            <div>
+                <SubTitle className="shadow-2xl">
+                    <span className="text-red-400"> problema </span>
+                    →
+                    <span className="text-blue-400"> solução </span>
+                    →
+                    <span className="text-green-400"> resultado </span>
+                </SubTitle>
+            </div>
+            <div className="flex justify-around place-content-start">
+                <GenericButton text="Contatos" onPress={() => goTo("/contatos")} />
+                <GenericButton text="Projetos" onPress={() => goTo("/projetos")} />
             </div>
         </div>
     )
 }
+
+// const LeftContainer = () => {
+
+//     const container = useRef<HTMLDivElement>(null);
+//     const boxRef = useRef<HTMLDivElement>(null);
+
+//     useGSAP(() => {
+//         gsap.to(boxRef.current, {
+//             rotation: 720,
+//             repeat: -10,
+//             duration: 50,
+//         });
+//     }, { scope: container });
+
+//     return (
+//         <div className="
+//                     flex flex-col sm:flex-col 
+//                     sm:justify-center 
+//                     gap-5 
+//                     sm:h-[70%] 
+//                     pt-10 sm:pl-5">
+//             <div className="self-center" ref={container}>
+//                 <div className="w-50 h-50 bg-amber-400" ref={boxRef} />
+//             </div>
+//         </div>
+//     )
+// }
