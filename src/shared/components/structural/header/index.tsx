cdc89863ffeaf19fc3440b78e50ca649050e1a36
Eliedson <goncalveseliedson@gmail.com>
2026-08-05 20:31:21 -0300

    feat: new toggle theme button style, header optimization and package imports updated

--- src/shared/components/structural/header/index.tsx
+++ src/shared/components/structural/header/index.tsx
@@ -1,6 +1,6 @@
 'use client'
-import ToggleTheme from "../../ui/buttons/toggleThemeBtn";
-import { Key, ReactNode, useState } from "react";
+import ToggleTheme from "../../ui/buttons/toggleTheme/toggleThemeBtn";
+import { ReactNode, useState } from "react";
 import { ResponsiveText } from "../../ui/text/responsiveText";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -26,11 +26,8 @@ export default function Header() {
     return (
         <ThemeProvider>
             <HeaderLayout>
-                <div className="flex p-1 gap-5 justify-evenly">
+                <div className="flex p-1 gap-5 justify-around">
                     <NavButtons />
-                </div>
-                <div className="self-center">
-                    <ToggleTheme />
                 </div>
             </HeaderLayout>
         </ThemeProvider>
@@ -45,22 +42,23 @@ const HeaderLayout = ({ children }: { children: ReactNode }) => {
         <header className="
         absolute
         w-full
-        justify-between
-        "
-        ><div
-        className="
-        my-2 mx-2
-        border rounded-xl
-        border-border-subcont
-        flex
-        sm:text-[0.5rem] md:text-[1rem] lg:text-md
-        h-10 
-        justify-end
-        backdrop-blur-md
+        justify-around
         "
         >
-            {children}
-        </div>
+            <div
+                className="
+                my-2 mx-2
+                border rounded-xl
+                border-border-subcont
+                flex
+                sm:text-[0.5rem] md:text-[1rem] lg:text-md
+                h-10 
+                justify-end
+                backdrop-blur-md
+                "
+            >
+                {children}
+            </div>
         </header>
     )
 }
@@ -71,21 +69,20 @@ const NavButtons = () => {
     const [icon] = useState<ReactNode>((theme === 'light' ? <FaHome className="self-center text-text group-hover:text-text-secondary uppercase" /> : <FaHome className="self-center text-text group-hover:text-text-secondary uppercase" />))
 
     return (
-        <nav className="flex justify-start">
-            <div className=" flex justify-evenly">
-                {pathname != '/' &&
-                    <HomeButtonBox href={'/'}>
-                        {icon}
-                    </HomeButtonBox>
-                    }
-                {menuData.map((item) => (
-                    <ButtonLayout key={item.title} href={item.link}>
-                        <ResponsiveText textSize="tiny" className={`self-center group-hover:text-text-secondary uppercase px-3 ${pathname === item.link ? 'border-b border-border-subcont text-text-secondary' : ''}`}>
-                            {item.title}
-                        </ResponsiveText>
-                    </ButtonLayout>
-                ))}
-            </div>
+        <nav className="flex justify-around mx-3">
+            {pathname != '/' &&
+                <HomeButtonBox href={'/'}>
+                    {icon}
+                </HomeButtonBox>
+            }
+            {menuData.map((item) => (
+                <ButtonLayout key={item.title} href={item.link}>
+                    <ResponsiveText textSize="tiny" className={`self-center group-hover:text-text-secondary uppercase px-3 ${pathname === item.link ? 'border-b border-border-subcont text-text-secondary' : ''}`}>
+                        {item.title}
+                    </ResponsiveText>
+                </ButtonLayout>
+            ))}
+            <ToggleTheme/>
         </nav>
     )
 }
@@ -95,7 +92,7 @@ const ButtonLayout = ({ children, href }: { children: ReactNode, href: string })
         <Link
             href={href}
             className="
-                px-5
+                mx-5
                 self-center
                 group
                 ">
@@ -106,7 +103,7 @@ const ButtonLayout = ({ children, href }: { children: ReactNode, href: string })
 
 const HomeButtonBox = ({ children, href }: { children: ReactNode, href: string }) => {
     return (
-        <Link  href={href} className="
+        <Link href={href} className="
             h-8 w-8
             flex flex-col
             justify-center items-center self-center
---
+++ src/shared/components/ui/buttons/toggleTheme/toggleThemeBtn.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useTheme } from "@/context/themeContext";
+import { ReactNode, useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import './style.css'
+
+export default function ToggleTheme() {
+    return (
+        <div className="flex place-self-center transition-all hover:cursor-pointer">
+            <ToggleButton />
+        </div>
+    )
+}
+
+const ToggleButton = () => {
+    const { theme, toggleTheme } = useTheme();
+    const [spining, setSpining] = useState<boolean>(false);
+    function changeTheme() {
+        setSpining(true);
+        toggleTheme();
+
+        setTimeout(() => {
+            setSpining(false)            
+        }, 1000);
+    }
+
+    return (
+        <button
+            onClick={changeTheme}
+            className={
+                spining ?
+                "btn-theme-style *:animate-[spin_1s_linear] *:hover:scale-105 *:transition-all"
+                :
+                 "btn-theme-style *:hover:scale-105 *:transition-transform"
+            }
+        >
+
+            <FaSun className={theme == "light" ? "show text-icon" : "hide"} />
+
+            <FaMoon className={theme == "dark" ? "show text-icon" : "hide"} />
+        </button>
+    )
+}
